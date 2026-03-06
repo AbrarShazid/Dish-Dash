b5bdc9e32ec9dc5b5c4fc441bf0919d8cad9f534
AbrarShazid <abraragmain75@gmail.com>
2026-03-07 04:01:20 +0600

    .

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,10 +1,22 @@
 
-import { env } from "@/env"
-import { createAuthClient } from "better-auth/react"
-const BACKEND_URL=env.NEXT_PUBLIC_BACKEND_URL
+// import { env } from "@/env"
+// import { createAuthClient } from "better-auth/react"
+// const BACKEND_URL=env.NEXT_PUBLIC_BACKEND_URL
 
+// export const authClient = createAuthClient({
+//     /** The base URL of the server (optional if you're using the same domain) */
+//     //  baseURL: "http://localhost:5000",
+//      baseURL: `${BACKEND_URL}`,
+// })
+
+
+
+import { env } from "@/env";
+import { createAuthClient } from "better-auth/react";
+const BACKEND_URL=env.NEXT_PUBLIC_BACKEND_URL
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    //  baseURL: "http://localhost:5000",
-     baseURL: `${BACKEND_URL}`,
-})
+baseURL: typeof window !== "undefined" ? window.location.origin : `${BACKEND_URL}`,
+fetchOptions: {
+credentials: "include",
+},
+});
