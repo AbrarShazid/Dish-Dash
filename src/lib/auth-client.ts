32282aeccb3ede04ab1e1a3295d0b6123a5fe333
AbrarShazid <abraragmain75@gmail.com>
2026-03-07 02:25:39 +0600

    .

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,10 @@
 
+import { env } from "@/env"
 import { createAuthClient } from "better-auth/react"
+const BACKEND_URL=env.NEXT_PUBLIC_BACKEND_URL
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-     baseURL: "http://localhost:5000",
+    //  baseURL: "http://localhost:5000",
+     baseURL: `${BACKEND_URL}`,
 })
